4f419028b447f18c8c6aa19ec39550a8ecdb09b8
Lane Goldberg <lanegoldberg@gmail.com>
2014-06-26 17:47:08 -0400

    prevent "double" transforms (caused by watchify)
    when using watchify, required templates get compiled as needed up the require stream. 
    
    in the wild: 
    
    ``` 
    // file: app.js
    module.exports = function({
      require('template.ejs)
    })
    ```
    
    using watchify, a change to `template.ejs` would trigger a transform of `template.ejs`, and then of `app.js`. at that point everything gets messed up.

--- index.js
+++ index.js
@@ -11,13 +11,18 @@ module.exports = function(file, options) {
   return through(function write(chunk) {
     buffer += chunk
   }, function end() {
-    var template = ejs.compile(buffer, {
-        client: true
-      , compileDebug: options.debug || false
-      , filename: file
-    })
-
-    this.queue('module.exports = (' + template + ')')
+    if (buffer.indexOf('module.exports') === 0) {
+      this.queue(buffer); // prevent "double" transforms 
+    } else {
+      
+      var template = ejs.compile(buffer, {
+          client: true
+        , compileDebug: options.debug || false
+        , filename: file
+      })
+  
+      this.queue('module.exports = (' + template + ')')
+    }
     this.queue(null)
   })
 }
